8d9565c2b83e6822f910e6fd4be524ecaf8b7034
Ian Maddison <ian@ianjm.com>
2015-03-15 20:17:56 +0000

    Updates

--- script.js
+++ script.js
@@ -1,45 +1,3 @@
-$(window).load(function() {
-    $("body").removeClass("noscript");
-    // $('section').removeClass('focus');
-});
-
-// $(document).ready(function() {
-//     $(document).scroll(function() {
-//         var scrollTop = $(document).scrollTop();
-//
-//         if (scrollTop > 150) {
-//             $('header.expanded').switchClass('expanded', 'collapsed', 350);
-//         }
-//         else {
-//             $('header.collapsed').switchClass('collapsed', 'expanded', 350);
-//         }
-//
-//         // $('article').each(function(i) {
-//         //     // is the article underneath the top header?
-//         //     if ($(this).offset().top < (scrollTop + $('header').outerHeight())) {
-//         //         var sticky = $(this).find('.sticky');
-//         //
-//         //         // only go as low as the top of the h4
-//         //         if ((sticky.offset().top + sticky.outerHeight()) < $(this).find('h4').offset().top) {
-//         //             $(this).find('.sticky').css('top', (scrollTop + $('header').outerHeight()) - $(this).offset().top );
-//         //         }
-//         //         // }
-//         //         // else {
-//         //             // alert('boo');
-//         //         // }
-//         //         // $(this).children('h1').css('top', $(this).outerHeight() - $(this).children('h1').outerHeight() );
-//         //     }
-//         //     else {
-//         //         $(this).find('.sticky').css('top', '0px');
-//         //     }
-//         // });
-//     });
-// });
-
-
-
-
-
 var word_array = [
     {text: "Dolor", weight: 6, html: {title: "I can haz any html attribute"}},
     {text: "Sit", weight: 7},
@@ -51,5 +9,62 @@ var word_array = [
 ];
 
 $(function() {
-  $("#cloud").jQCloud(word_array);
+    $("#cloud").jQCloud(word_array);
+    
+    $('section.panel').click(function() {
+        if (!$(this).hasClass('focus')) {
+            $('section.panel').switchClass('focus', 'unfocus', {
+                duration: 200
+            });
+            
+            $(this).switchClass( 'unfocus', 'focus', {
+                duration: 200
+            });
+        }
+    });
 });
+
+
+$(document).scroll(function() {
+    var scrollTop = $(document).scrollTop();
+    if (scrollTop > 150) {
+        $('header.expanded').switchClass('expanded', 'collapsed', {
+            duration: 350
+        });
+    }
+    else {
+        $('header.collapsed').switchClass('collapsed', 'expanded', {
+            duration: 350,
+            complete: function() {
+                $('section.panel').removeClass('focus');
+            }
+        });
+    }
+    
+    $('section.panel').switchClass('focus', 'unfocus', {
+        duration: 200
+    });
+});
+
+
+
+        // $('article').each(function(i) {
+        //     // is the article underneath the top header?
+        //     if ($(this).offset().top < (scrollTop + $('header').outerHeight())) {
+        //         var sticky = $(this).find('.sticky');
+        //
+        //         // only go as low as the top of the h4
+        //         if ((sticky.offset().top + sticky.outerHeight()) < $(this).find('h4').offset().top) {
+        //             $(this).find('.sticky').css('top', (scrollTop + $('header').outerHeight()) - $(this).offset().top );
+        //         }
+        //         // }
+        //         // else {
+        //             // alert('boo');
+        //         // }
+        //         // $(this).children('h1').css('top', $(this).outerHeight() - $(this).children('h1').outerHeight() );
+        //     }
+        //     else {
+        //         $(this).find('.sticky').css('top', '0px');
+        //     }
+        // });
+
